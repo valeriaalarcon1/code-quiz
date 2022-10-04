e9b9b46289df7fd00229d9bc10543db4384c1567
Valeria <valeriaalarcon@Valerias-MacBook-Pro.local>
2022-10-04 09:48:32 -0400

    Added general outline and notes of JS code

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+//          DEPENDENCIES (DOM ELEMENTS)           //
+
+// start quiz button
+// timer element
+// answer buttons
+// submit initials button
+// go back button
+// clear high scores button
+// view high scores button
+
+
+
+
+
+//                     DATA                       //
+
+
+
+
+
+
+//                  FUNCTIONS                     //
+
+// display start screen
+// when start quiz button clicked, go to next page -> questions
+// start timer
+// display question
+// when answer buttton clicked, display correct or wrong & go to next page -> next question
+// if get 3 wrong OR if get all right -> display all done/game over screen
+// timer stops
+// when initials written and submit button clicked, go to next page -> high scores page
+// when clear high scores screen, clear it
+// when go back button clicked, return to start -> start quiz screen
+
+
+
+
+
+
+//               USER INTERACTIONS                //
+
+// user clicks start quiz button
+// user clicks answers
+// user types and submits initials for high score
+// user clicks clear high scores (optional)
+// user clicks go back
+
+
+
+
+
+
+//                INITIALIZATION                  //
