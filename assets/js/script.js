c009b5ed150240437cf486ef97280abda745c998
Valeria <valeriaalarcon@Valerias-MacBook-Pro.local>
2022-10-04 21:26:23 -0400

    Added code to Javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,48 @@
 //          DEPENDENCIES (DOM ELEMENTS)           //
 
+var empty = ''
 // start quiz button
+var startButton = document.getElementById('start-button');
+// Start Screen
+var startScreen = document.querySelector('.start-screen');
 // timer element
-// answer buttons
+var timer = document.querySelector('.timer');
+// question box
+var questionBox = document.querySelector('.question-box');
+questionBox.setAttribute("style", "display: none");
+// game over screen
+var gameOverScreen = document.querySelector('game-over');
+// high scores screen
+var highScoresScreen = document.querySelector('.high-scores');
 // submit initials button
+var submitButton = document.getElementById('submit-button');
 // go back button
+var goBackButton = document.getElementById('go-back-button');
 // clear high scores button
+var clearButton = document.getElementById('clear-button');
 // view high scores button
-
-
-
+var viewButton = document.getElementById('view-button');
+// Question & Answer
+var currentQuestion = 0
 
 
 //                     DATA                       //
 // number of seconds
-// questions
-// right answers
-// wrong answers 
+var secondsLeft = 75;
+
+// questions and answers
+var questions = [
+    {
+        question: 'here is a question?',
+        options: ["a", "b", "c", "d"],
+        answer: "c"
+    },
+    {
+        question: 'another question?',
+        options: ["1", "2", "3", "4"],
+        answer: "2"
+    }
+]
 
 
 
@@ -26,8 +52,51 @@
 
 // display start screen
 // when start quiz button clicked, go to next page --> questions
+startButton.addEventListener("click", function(event) {
+    console.log("Start Game");
+    startScreen.setAttribute("hidden", "true");
+    startGame();
+    // shuffleQuestions(questions);
+    // showQuestion();
+    setTime();
+});
+
+function startGame() {
+    questionBox.setAttribute("style", "display: block");
+//     startButton.classList.add('hide');
+//     randomQuestions = questions.sort(() => Math.random() - .5);
+//     currentQuestionIndex = 0;
+//     currentQuestion.classList.remove('hide');
+}
+
 // start timer
+function setTime() {
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timer.innerHTML = "Timer: " + secondsLeft;
+        if (secondsLeft === 0) {
+            clearInterval(timerInterval);
+        }
+    }, 1000); 
+}
+
 // display question
+function renderQuestion(question) {
+    currentQuestion.textContent = question;
+}
+
+function renderOptions() {
+    answerOptions.append(options);
+}
+
+function renderQuiz(questions, index) {
+    var Q = questions[index];
+    renderQuestion(Q.question);
+    renderOptions(Q.options);
+    console.log("Question");
+    console.log(questions[index]);
+}
+
 // when answer buttton clicked
     // if incorrect, reduce time & display wrong --> next question
     // if correct, display correct --> next question
